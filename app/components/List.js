0fd6a7f503cf754712dfe3502271c8e4639d0b96
Paz Marzana <pazmarzana@gmail.com>
2021-11-24 00:11:03 -0300

    agregado buscador ciudades y  chequea que sea distinta la ciudad

--- app/components/List.js
+++ app/components/List.js
@@ -3,101 +3,138 @@ import { Icon } from 'react-native-elements'
 import { View, Text, StyleSheet, FlatList, ImageBackground, TouchableOpacity} from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useIsFocused } from '@react-navigation/native';
+import SearchBar from './SearchBar';
 
 const List = ({navigation}) => {
   const[ciudadesGuardadas, setCiudadesGuardadas] = useState([{}]);
-    const isFocused = useIsFocused();
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
 
-    useEffect(() => {
-        getData();
-    },[isFocused]);
+  const isFocused = useIsFocused();
 
-    const storeData = async (value) => {
-        try {
-          const jsonValue = JSON.stringify(value)
-          await AsyncStorage.setItem('ciudades', jsonValue)
-        } catch (e) {
-          console.log("error al guardar los datos");
-          console.log(e);
-        }
-      }
+  useEffect(() => {
+      getData();
+  },[isFocused]);
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('ciudades', jsonValue)
+    } catch (e) {
+      console.log("error al guardar los datos");
+      console.log(e);
+    }
+  }
     
-      const getData = async () => {
-        try {
-          const jsonValue = await AsyncStorage.getItem('ciudades')
-          if( jsonValue != null ) {
-            // console.log("------ en List ---------");  
-            // console.log(jsonValue);
-            setCiudadesGuardadas(JSON.parse(jsonValue));
-            // console.log(JSON.parse(jsonValue));
-          }else {
-            console.log("no hay ciudades guardadas");
-          } 
-        } catch(e) {
-          console.log("error al leer los datos");
-          console.log(e);
-        }
-      }
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('ciudades')
+      if( jsonValue != null ) {
+        setCiudadesGuardadas(JSON.parse(jsonValue));
+      }else {
+        console.log("no hay ciudades guardadas");
+      } 
+    } catch(e) {
+      console.log("error al leer los datos");
+      console.log(e);
+    }
+  }
 
-      const eliminarCiudad = (ItemId) => {
-        // console.log("desde eliminar ciudadesGuardadas");
-        // console.log(ItemId);
-        setCiudadesGuardadas( (ciudadesGuardadas) => {
-            return ciudadesGuardadas.filter( ciudad => (ciudad.id != ItemId) );
-        })
-        storeData(ciudadesGuardadas);
-        // console.log("desde eliminar ciudadesGuardadas");
-        // console.log(ciudadesGuardadas);
-      };
+  const eliminarCiudad = (ItemId) => {
+    setCiudadesGuardadas( (ciudadesGuardadas) => {
+      return ciudadesGuardadas.filter( ciudad => (ciudad.id != ItemId) );
+    })
+    storeData(ciudadesGuardadas);
+  };
 
-    const renderItem = ({ item }) => (
-        <View style={styles.itemsBox} key={`${item.id}lista`}>
-            <View style={styles.listaItem} key={`${item.id}bloque`}>
-                <Text style={styles.listaItemName} key={`${item.id}name`}>{item.name}</Text>
-                <TouchableOpacity
-                    style={styles.listaItemButton} 
-                    onPress={()=> {navigation.navigate('Clima',{lat: item.lat, lon: item.lon, name: item.name });}}
-                    key={`${item.id}clima`}
-                    >
-                    <Icon name="weather-cloudy" type='material-community' color='#FFF' key={`${item.id}icon1`}/>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.listaItemButton} 
-                    onPress={()=> {eliminarCiudad(item.id)}}
-                    key={`${item.id}eliminar`}
-                    >
-                    <Icon name="trash-can-outline" type='material-community' color='#FFF' key={`${item.id}icon2`}/>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
-    
-    const ciudadesAMostrar = ciudadesGuardadas.filter(item => item.hasOwnProperty('id'));
-
+  const renderItem = ({ item }) => {
+  // si no tengo que aplicar filtro 
+  if (searchPhrase === "") {
     return (
-        <View style={styles.container}>
-            <ImageBackground source={require('../assets/img/background.jpg')} style={styles.img}>
-                <View style={styles.container_title}>
-                    <Text style={styles.title}>Listado de ciudades</Text>
-                </View>
-                
-                <FlatList style={styles.container_flat}
-                    data={ciudadesAMostrar}
-                    renderItem={renderItem}
-                    keyExtractor={ciudad => ciudad.id}
-                />
+      <View style={styles.itemsBox} key={`${item.id}lista`}>
+        <View style={styles.listaItem} key={`${item.id}bloque`}>
+          <Text style={styles.listaItemName} key={`${item.id}name`}>{item.name}</Text>
+          <TouchableOpacity
+            style={styles.listaItemButton} 
+            onPress={()=> {navigation.navigate('Clima',{lat: item.lat, lon: item.lon, name: item.name });}}
+            key={`${item.id}clima`}
+            >
+            <Icon name="weather-cloudy" type='material-community' color='#FFF' key={`${item.id}icon1`}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.listaItemButton} 
+            onPress={()=> {eliminarCiudad(item.id)}}
+            key={`${item.id}eliminar`}
+            >
+            <Icon name="trash-can-outline" type='material-community' color='#FFF' key={`${item.id}icon2`}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+  // si ingresaron una ciudad en el buscador 
+  if (item.name.toUpperCase().includes(searchPhrase.toUpperCase())) {
+    return (
+      <View style={styles.itemsBox} key={`${item.id}lista`}>
+        <View style={styles.listaItem} key={`${item.id}bloque`}>
+          <Text style={styles.listaItemName} key={`${item.id}name`}>{item.name}</Text>
+          <TouchableOpacity
+            style={styles.listaItemButton} 
+            onPress={()=> {navigation.navigate('Clima',{lat: item.lat, lon: item.lon, name: item.name });}}
+            key={`${item.id}clima`}
+            >
+            <Icon name="weather-cloudy" type='material-community' color='#FFF' key={`${item.id}icon1`}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.listaItemButton} 
+            onPress={()=> {eliminarCiudad(item.id)}}
+            key={`${item.id}eliminar`}
+            >
+            <Icon name="trash-can-outline" type='material-community' color='#FFF' key={`${item.id}icon2`}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 
-                <TouchableOpacity 
-                    style={styles.btn}
-                    activeOpacity={0.6}
-                    onPress = {() => navigation.navigate('Search')}    
-                > 
-                    <Icon name="plus" type='material-community'/>
-                    
-                </TouchableOpacity>
-            </ImageBackground>    
-        </View> 
-    );
+};
+
+const ciudadesAMostrar = ciudadesGuardadas.filter(item => item.hasOwnProperty('id'));
+
+return (
+  <View style={styles.container}>
+    <ImageBackground source={require('../assets/img/background.jpg')} style={styles.img}>
+      {/* si esta el buscador no escribo el titulo  */}
+      {!clicked  && (<View style={styles.container_title}>
+        <Text style={styles.title}>Listado de ciudades</Text>
+      </View>)}
+      <SearchBar style={styles.searchBar}
+        clicked={clicked}
+        setClicked={setClicked}
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        />
+      <View style={styles.container_flat}
+        onStartShouldSetResponder={() => {
+          setClicked(false);
+        }}
+      >
+        <FlatList  style={styles.flatList}
+          data={ciudadesAMostrar}
+          renderItem={renderItem}
+          keyExtractor={ciudad => ciudad.id}
+          />
+      </View>   
+      <TouchableOpacity 
+          style={styles.btn}
+          activeOpacity={0.6}
+          onPress = {() => navigation.navigate('Search')}    
+      > 
+          <Icon name="plus" type='material-community'/>
+      </TouchableOpacity>
+    </ImageBackground>    
+  </View> 
+  );
 };
 
 
@@ -124,6 +161,13 @@ const styles = StyleSheet.create({
     container_flat:{
         marginBottom: 20,
         width: '80%',
+        // width: '100%',
+    },
+    flatList:{
+      // marginBottom: 20,
+    },
+    searchBar:{
+      // width: '60%'
     },
     itemsBox:{
         // backgroundColor: '#F21',
@@ -168,15 +212,20 @@ const styles = StyleSheet.create({
     btn: {
         backgroundColor: '#3098DB',
         justifyContent: 'center',
+
         borderRadius: 100,
         borderWidth: 2,
         height: 70,
         width: 70,
         alignSelf: 'flex-end',
+        // alignSelf: 'flex-start',
+
         right: 20,
-        bottom: 120,
+        top: 200,
+        zIndex: 10,
+        // bottom: 120,
         shadowColor: "#000",
-        elevation: 10
+        elevation: 30
       },
 })
 
